8bfb319c0b1570182f82fb989565cf68a1c73200
taiz <walter.kerly@protonmail.com>
2024-07-04 17:58:32 -0400

    Init

---
+++ main.py
@@ -0,0 +1,47 @@
+"""Main module"""
+
+import json
+import urllib.request
+
+BASE_API_LINK = "https://hacker-news.firebaseio.com/v0"
+HIRING_BOT_ID = "whoishiring"
+
+
+def link_to_json(link: str):
+    """Get a link, convert it to a JSON dict, and return it"""
+    request = urllib.request.Request(link)
+    with urllib.request.urlopen(request) as response:
+        return json.loads(response.read())
+
+
+def get_user_thread_link(thread_id: str):
+    """Give a thread id, get a link to the real HN site"""
+    return f"https://news.ycombinator.com/item?id={thread_id}"
+
+
+def get_hiring_threads(thread_count: int = None):
+    """Returns a list of thread id"""
+    json_link = f"{BASE_API_LINK}/user/{HIRING_BOT_ID}.json"
+    return link_to_json(json_link)["submitted"][:thread_count]
+
+
+def get_top_level_comments(thread_id: str, child_count: int = None):
+    """Returns a list of direct children IDs on a post"""
+    parent_link = f"{BASE_API_LINK}/item/{thread_id}.json"
+    return link_to_json(parent_link)["kids"][:child_count]
+
+
+def get_thread_desc(thread_id: str):
+    """Gets a thread's description"""
+    parent_link = f"{BASE_API_LINK}/item/{thread_id}.json"
+    return link_to_json(parent_link)["text"]
+
+
+def determine_job_skills(comment_body: str):
+    pass
+
+
+threads = get_hiring_threads(10)
+comments = get_top_level_comments(threads[0])
+print(get_thread_desc(comments[0]))
+# print(get_hiring_threads(10))
